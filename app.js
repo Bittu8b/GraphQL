f805669c8acb56774e7428e3ef19dc937dd8d0cc
Saubhik Singh <GSC-30349@GSLAB.COM>
2019-03-26 13:53:26 +0530

    core backend

--- app.js
+++ app.js
@@ -1,78 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
-const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 
-const Event = require("./Models/event.js");
+const graphQlSchema = require("./GraphQL/Schema/index.js");
+const graphQlResolver = require("./GraphQL/Resolver/index.js");
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: buildSchema(`
-        type Event{
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input EventInput{
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-
-        }
-
-        type RootQuery{
-            events: [Event!]!
-
-        }
-        type RootMutation{
-            createEvents(eventInput: EventInput): Event
-
-        }
-        schema{
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              return { ...event._doc };
-            });
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      },
-      createEvents: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
-
-        return event
-          .save()
-          .then(result => {
-            console.log("Insertion Successful " + result);
-            return { ...result._doc };
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolver,
     graphiql: true
   })
 );
